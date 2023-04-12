f6f60bca08228ce0e8e42fe91a9f12823bc1ec17
shabilullah <shabilullah@gmail.com>
2023-04-12 15:06:46 +0800

    app: update to fix out-of-date code
    Signed-off-by: shabilullah <shabilullah@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-
 import gradio as gr 
 import os
-import sys
 import subprocess
-#from moviepy.editor import VideoFileClip
-
 import whisper
-from whisper.utils import write_vtt
 
 model = whisper.load_model("medium")
+
+output_dir = "output"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+from whisper.utils import get_writer
 
 def video2mp3(video_file, output_ext="mp3"):
     filename, ext = os.path.splitext(video_file)
@@ -17,43 +18,27 @@ def video2mp3(video_file, output_ext="mp3"):
                     stderr=subprocess.STDOUT)
     return f"{filename}.{output_ext}"
 
-
 def translate(input_video):
-
     audio_file = video2mp3(input_video)
-    
     options = dict(beam_size=5, best_of=5)
     translate_options = dict(task="translate", **options)
     result = model.transcribe(audio_file,**translate_options)
-
-    output_dir = '/content/'
-    audio_path = audio_file.split(".")[0]
-
-    with open(os.path.join(output_dir, audio_path + ".vtt"), "w") as vtt:
-      write_vtt(result["segments"], file=vtt)
-
-    subtitle = audio_path + ".vtt"
-    output_video = audio_path + "_subtitled.mp4"
-
-    os.system(f"ffmpeg -i {input_video} -vf subtitles={subtitle} {output_video}")
-
+    audio_filename = os.path.splitext(os.path.basename(audio_file))[0]
+    writer = get_writer("vtt", output_dir)
+    writer(result, audio_filename)
+    subtitle = os.path.join(output_dir, audio_filename + ".vtt").replace("\\", "/")
+    output_video = os.path.join(output_dir, audio_filename + "_subtitled.mp4").replace("\\", "/")
+    os.system(f"ffmpeg -y -i {input_video} -vf subtitles={subtitle} {output_video}")
     return output_video
-
-
 
 title = "Add Text/Caption to your YouTube Shorts - MultiLingual"
 
 block = gr.Blocks()
 
 with block:
-
     with gr.Group():
         with gr.Box(): 
-
-           
-
             with gr.Row().style():
-               
                 inp_video = gr.Video(
                     label="Input Video",
                     type="filepath",
@@ -61,18 +46,11 @@ with block:
                 )
                 op_video = gr.Video()
             btn = gr.Button("Generate Subtitle Video")
-        
-        
-        
-
-
-        
         btn.click(translate, inputs=[inp_video], outputs=[op_video])
- 
         gr.HTML('''
         <div class="footer">
-                    <p>Model by <a href="https://github.com/openai/whisper" style="text-decoration: underline;" target="_blank">OpenAI</a> - Gradio App by <a href="https://twitter.com/1littlecoder" style="text-decoration: underline;" target="_blank">1littlecoder</a>
-                    </p>
+            <p>Model by <a href="https://github.com/openai/whisper" style="text-decoration: underline;" target="_blank">OpenAI</a> - Gradio App by <a href="https://twitter.com/1littlecoder" style="text-decoration: underline;" target="_blank">1littlecoder</a>
+            </p>
         </div>
         ''')
 
